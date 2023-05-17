57bf774afd8d20451ccb63f1908773d3a080b0ae
Yonas Marie~ <yonasgebre08@gmail.com>
2023-05-17 15:03:04 +0300

    feat(Backend-Yonas) create mapping

---
+++ TaskManagementSystem.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using TaskManagementSystem.Application.Features.CheckList.DTO;
+using TaskManagementSystem.Application.Features.Task.DTO;
+using TaskManagementSystem.Application.Features.User.DTO;
+using TaskManagementSystem.Domain;
+
+namespace TaskManagementSystem.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        #region  User
+        CreateMap<User, CreateUserDto>().ReverseMap();
+        CreateMap<User, UpdateUserDto>().ReverseMap();
+        CreateMap<User, GetUserListDto>().ReverseMap();
+        CreateMap<User, GetUserDetailDto>().ReverseMap();
+        #endregion
+
+        #region  UserTask
+        CreateMap<UserTask, CreateUserTaskDto>().ReverseMap();
+        CreateMap<UserTask, UpdateUserTaskDto>().ReverseMap();
+        CreateMap<UserTask, GetUserTaskListDto>().ReverseMap();
+        CreateMap<UserTask, GetUserTaskDetailDto>().ReverseMap();
+        #endregion
+
+        #region  CheckList
+        CreateMap<CheckList, CreateCheckListDto>().ReverseMap();
+        CreateMap<CheckList, UpdateCheckListDto>().ReverseMap();
+        CreateMap<CheckList, GetCheckListListDto>().ReverseMap();
+        CreateMap<CheckList, GetCheckListDetailDto>().ReverseMap();
+        #endregion
+
+    }
+}

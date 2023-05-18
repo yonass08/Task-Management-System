f6ef21c2c68ebbd6fc5c163507558b23c38eb227
Yonas Marie~ <yonasgebre08@gmail.com>
2023-05-18 12:19:10 +0300

    feat(Backend-Yonas) api layer

---
+++ TaskManagementSystem.Api/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+using TaskManagementSystem.Application.Features.User.CQRS.Requests.Commands;
+using TaskManagementSystem.Application.Features.User.CQRS.Requests.Queries;
+using TaskManagementSystem.Application.Features.User.DTO;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace TaskManagementSystem.API.Controllers;
+
+ 
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public UserController(IMediator mediatR)
+    {
+        _mediator = mediatR;     
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<List<GetUserListDto>>> Get()
+    {
+        var Users = await _mediator.Send(new GetUserListQuery());
+        return Ok(Users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetUserDetailDto>> Get(int id)
+    {
+        var User = await _mediator.Send(new GetUserDetailQuery { Id = id });
+        return Ok(User);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateUserDto UserDto)
+    {
+        var response = await _mediator.Send(new CreateUserCommand{createUserDto = UserDto});
+        return Ok(response);   
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put([FromBody] UpdateUserDto UserDto)
+    {
+
+        await _mediator.Send(new UpdateUserCommand{updateUserDto = UserDto});
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var UserDto = new DeleteUserDto{ Id = id};
+        await _mediator.Send(new DeleteUserCommand{ deleteUserDto = UserDto});
+        return NoContent(); 
+    }
+}
+
---
+++ TaskManagementSystem.Api/Controllers/UserTaskController.cs
@@ -0,0 +1,66 @@
+using TaskManagementSystem.Application.Features.UserTask.CQRS.Requests.Commands;
+using TaskManagementSystem.Application.Features.UserTask.CQRS.Requests.Queries;
+using TaskManagementSystem.Application.Features.UserTask.DTO;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace TaskManagementSystem.API.Controllers;
+
+ 
+[ApiController]
+[Route("api/[controller]")]
+public class UserTaskController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public UserTaskController(IMediator mediatR)
+    {
+        _mediator = mediatR;     
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<List<GetUserTaskListDto>>> Get()
+    {
+        var UserTasks = await _mediator.Send(new GetUserTaskListQuery());
+        return Ok(UserTasks);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetUserTaskDetailDto>> Get(int id)
+    {
+        var UserTask = await _mediator.Send(new GetUserTaskDetailQuery { Id = id });
+        return Ok(UserTask);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateUserTaskDto UserTaskDto)
+    {
+        var response = await _mediator.Send(new CreateUserTaskCommand{createUserTaskDto = UserTaskDto});
+        return Ok(response);   
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put([FromBody] UpdateUserTaskDto UserTaskDto)
+    {
+
+        await _mediator.Send(new UpdateUserTaskCommand{updateUserTaskDto = UserTaskDto});
+        return NoContent(); 
+    }
+
+    [HttpPut("Status")]
+    public async Task<ActionResult> Put([FromBody] UpdateUserTaskStatusDto UserTaskDto)
+    {
+
+        await _mediator.Send(new UpdateUserTaskStatusCommand{updateUserTaskStatusDto = UserTaskDto});
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var UserTaskDto = new DeleteUserTaskDto{ Id = id};
+        await _mediator.Send(new DeleteUserTaskCommand{ deleteUserTaskDto = UserTaskDto});
+        return NoContent(); 
+    }
+}
+
